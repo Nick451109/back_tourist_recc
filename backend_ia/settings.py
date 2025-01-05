6fdf71d1e7cf16eb965296054ace156f4a58d411
Nick Arévalo <69706202+Nick451109@users.noreply.github.com>
2025-01-04 22:41:22 -0500

    allowed hosts .env

--- backend_ia/settings.py
+++ backend_ia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hij0@yld=fxgb8*#wr^!qwvch*^*+%bc6t&q7x50d)o&dte27^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
